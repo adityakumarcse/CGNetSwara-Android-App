88e9dbf19604d6cdf9ca93b77dd7a88440f93b96
Krittika D'Silva <krittika.dsilva@gmail.com>
2014-07-05 15:41:35 +0530

    Removed unused code in GetAudio.java

--- CGNet_IVR/src/com/MSRi/ivr/GetAudioFiles.java
+++ CGNet_IVR/src/com/MSRi/ivr/GetAudioFiles.java
@@ -1,127 +1,26 @@
 package com.MSRi.ivr;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URL;
-import java.net.URLConnection;
-
-import android.app.Activity;
-import android.app.Dialog;
-import android.app.ProgressDialog;
-import android.graphics.drawable.Drawable;
-import android.media.MediaPlayer;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log; 
-import android.widget.ImageView;
+import android.view.Menu;
+import android.app.Activity; 
 
 public class GetAudioFiles extends Activity {
 	private static final String TAG = "RecordAudio";
 	 
-    /** Progress Dialog */
-    private ProgressDialog pDialog;
-     
-    /** Progress dialog type (0 - for Horizontal progress bar) */
-    public static final int progress_bar_type = 0; 
- 
-    /** File url to download */
-    private static String file_url = "http://cgnetswara.org/sounds/270.wav";
- 
+    // TODO : This activity needs to be implemented.
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.get_audio);
-   
-        // Image view to show image after downloading
+        setContentView(R.layout.get_audio); 
+    }
  
 
-        new DownloadFileFromURL().execute(file_url);
-    }
- 
-    /**
-     * Showing Dialog
-     * */
+    /** Inflates the menu. Currently, there aren't any meaningful items
+     *  to add to the action bar. */
     @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-        case progress_bar_type: // we set this to 0
-            pDialog = new ProgressDialog(this);
-            pDialog.setMessage("Downloading file. Please wait...");
-            pDialog.setIndeterminate(false);
-            pDialog.setMax(100);
-            pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-            pDialog.setCancelable(true);
-            pDialog.show();
-            return pDialog;
-        default:
-            return null;
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
- 
-    /**
-     * Background Async Task to download file
-     * */
-    class DownloadFileFromURL extends AsyncTask<String, String, String> {
- 
-        /**
-         * Before starting background thread
-         * Show Progress Bar Dialog
-         * */
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            showDialog(progress_bar_type);
-        }
- 
-        /**
-         * Downloading file in background thread
-         * */
-        @Override
-        protected String doInBackground(String... f_url) {
-            int count;
-            try {
-                URL url = new URL(f_url[0]);
-                URLConnection conection = url.openConnection();
-                conection.connect();
-                // this will be useful so that you can show a tipical 0-100% progress bar
-                int lenghtOfFile = conection.getContentLength();
- 
-                // download the file
-                InputStream input = new BufferedInputStream(url.openStream(), 8192);
- 
-                // Output stream
-                OutputStream output = new FileOutputStream("/sdcard/downloadedfile.wav");
- 
-                byte data[] = new byte[1024];
- 
-                long total = 0;
- 
-                while ((count = input.read(data)) != -1) {
-                    total += count;
-                    // publishing the progress....
-                    // After this onProgressUpdate will be called
-                    publishProgress(""+(int)((total*100)/lenghtOfFile));
- 
-                    // writing data to file
-                    output.write(data, 0, count);
-                }
- 
-                // flushing output
-                output.flush();
- 
-                // closing streams
-                output.close();
-                input.close();
- 
-            } catch (Exception e) {
-                Log.e("Error: ", e.getMessage());
-            }
- 
-            return null;
-        }
-    }
+     
 }
