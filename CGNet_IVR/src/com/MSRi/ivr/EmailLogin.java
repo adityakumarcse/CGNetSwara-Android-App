5f7d0130ce19ac367bc0a569326ee783b938f3d2
Krittika D'Silva <krittika.dsilva@gmail.com>
2014-07-15 16:14:03 +0530

    Removed email details, changed password

--- CGNet_IVR/src/com/MSRi/ivr/EmailLogin.java
+++
@@ -1,9 +0,0 @@
-package com.MSRi.ivr;
-// Do not version control this file 
-
-public class EmailLogin {
-
-	public static final String email = "cgnet112358@gmail.com";
-	public static final String password = "cgnetswara";
-	//	public static final String google_
-}
